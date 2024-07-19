c6f4c8cb60a57d1046481d0c62709d97ab945b99
Šárka Flídrová <sarka.flidrova@email.cz>
2024-07-19 12:12:10 +0200

    com. List: Added basic style to the fetched items

--- src/List/index.jsx
+++ src/List/index.jsx
@@ -19,7 +19,22 @@ export const List = () => {
   return (
     <div className="container--list">
       <h2 className="list--title">Here is a list of your tasks:</h2>
-      {console.log(tasks)}
+      {tasks.map((task) => {
+        return (
+          <div className="list--content__task" key={task.id}>
+            <div className="list-content__task--header">{task.taskName}</div>
+            <div className="list-content__task--desc">
+              <p> Description: {task.taskDescription}</p>
+              <p> Tags: {task.tags}</p>
+            </div>
+            <div className="list-content__task--status">
+              <p>Status: {task.done === true ? "DONE" : "TO DO"}</p>
+              <p>Pritoriy: {task.priority}</p>
+              <p>Due Date: {task.dueDate}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
