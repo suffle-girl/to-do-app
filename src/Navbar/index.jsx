ac0a6090b667a3d4a31c6097bdbf5556815877ca
Šárka Flídrová <sarka.flidrova@email.cz>
2024-07-19 10:37:33 +0200

    com. Navbar: Adjusted layout

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -3,8 +3,10 @@ import "./style.css";
 export const Navbar = () => {
   return (
     <div className="container--navbar">
-      <h1>To Do App</h1>
-      <h3>Welcome to the To Do App!</h3>
+      <div className="navbar--text">
+        <h1>To Do App</h1>
+        <h3>Welcome to the To Do App!</h3>
+      </div>
       <div className="navbar--logo"></div>
     </div>
   );
