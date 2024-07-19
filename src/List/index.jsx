e0431d22347e94afe9ab5ade560236336518b062
Šárka Flídrová <sarka.flidrova@email.cz>
2024-07-19 11:54:00 +0200

    com. List: Added the fetch function

--- src/List/index.jsx
+++ src/List/index.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 
 export const List = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://669a16149ba098ed61fe4298.mockapi.io/todo/api/v1/tasks"
+      );
+      const data = await response.json();
+
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
+
   return (
     <div className="container--list">
       <h2 className="list--title">Here is a list of your tasks:</h2>
+      {console.log(tasks)}
     </div>
   );
 };
