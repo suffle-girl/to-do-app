91f8f46eb8cb53d91adf5e3c7445cd0e830665e0
Šárka Flídrová <sarka.flidrova@email.cz>
2024-07-20 14:58:06 +0200

    com. Navbar: Adjusted styles for media queries.

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -4,8 +4,8 @@ export const Navbar = () => {
   return (
     <div className="container--navbar">
       <div className="navbar--text">
-        <h1>To Do App</h1>
-        <h3>Welcome to the To Do App!</h3>
+        <h1 className="navbar--text__title">To Do App</h1>
+        <h3 className="navbar--text__subtitle">Welcome to the To Do App!</h3>
       </div>
       <div className="navbar--logo"></div>
     </div>
